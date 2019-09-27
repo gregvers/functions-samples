62a525bf2c9ab39a936974c08b13172fbf9c9892
gregvers <gregvers@gmail.com>
2019-09-26 20:55:19 -0700

    initial

---
+++ fn-list-compartments-python/func.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2019, Oracle and/or its affiliates.  All rights reserved.
+import io
+import json
+
+from fdk import response
+import oci.identity
+
+def handler(ctx, data: io.BytesIO = None):
+    signer = oci.auth.signers.get_resource_principals_signer()
+    resp = list_compartments(signer)  # function defined below
+    return response.Response(
+        ctx,
+        response_data=json.dumps(resp),
+        headers={"Content-Type": "application/json"}
+    )
+
+# List compartments ------------------------------------------------------------
+def list_compartments(signer):
+    client = oci.identity.IdentityClient(config={}, signer=signer)
+    # OCI API for managing users, groups, compartments, and policies
+    try:
+        # Returns a list of all compartments and subcompartments in the tenancy (root compartment)
+        compartments = client.list_compartments(
+            signer.tenancy_id,
+            compartment_id_in_subtree=True,
+            access_level='ANY'
+        )
+        # Create a list that holds a list of the compartments id and name next to each other
+        compartments = [[c.id, c.name] for c in compartments.data]
+    except Exception as e:
+        compartments = str(e)
+    resp = {"compartments": compartments}
+    return resp
---
+++ fn-list-instances-python/func.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2019, Oracle and/or its affiliates.  All rights reserved.
+import io
+import json
+from fdk import response
+
+import oci
+
+def handler(ctx, data: io.BytesIO=None):
+    signer = oci.auth.signers.get_resource_principals_signer()
+    resp = list_instances(signer)
+    return response.Response(
+        ctx,
+        response_data=json.dumps(resp),
+        headers={"Content-Type": "application/json"}
+    )
+
+# List instances ---------------------------------------------------------------
+def list_instances(signer):
+    client = oci.core.ComputeClient(config={}, signer=signer)
+    # OCI API to manage Compute resources such as compute instances, block storage volumes, etc.
+    try:
+        # Returns a list of all instances in the current compartment
+        inst = client.list_instances(signer.compartment_id)
+        # Create a list that holds a list of the instances id and name next to each other
+        inst = [[i.id, i.display_name] for i in inst.data]
+    except Exception as e:
+        inst = str(e)
+    resp = { "instances": inst }
+    return resp
